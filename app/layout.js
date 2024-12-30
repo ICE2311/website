ab9c1dbe85aa1e1ee522e8e16ba8a21ee14a7e27
ice2311 <icyzoid@gmail.com>
2024-12-30 11:21:34 +0530

    vercel speed insight

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "@/components/SessionWrapper";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SessionWrapper>{children}</SessionWrapper>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SessionWrapper>
+          {children}
+          <SpeedInsights />
+        </SessionWrapper>
       </body>
     </html>
   );
